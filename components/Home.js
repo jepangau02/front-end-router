d105a54946d0d24b75c83f35dafd71b11045beed
jepangau02 <s11920004@student.unklab.ac.id>
2023-10-29 16:45:29 +0800

    commit saya

---
+++ components/Home.js
@@ -0,0 +1,9 @@
+//default page
+function Home (){
+    return (
+        <div>
+            <h1>Welcome to the class front end !!!</h1>
+            <h3>How are you today?</h3>
+        </div>
+    );
+}
